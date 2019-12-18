90665a4109de7c7c55855b899ad1fc872a385443
Suleyman Selim <suleyman.selim@mail.com>
2019-12-18 23:19:49 +0000

    Update autodock.sh

--- autodock.sh
+++ autodock.sh
@@ -23,11 +23,13 @@ NR_CPUS=8		#Number of CPU
 xyz=$(eBoxSize.pl ref-lig.mol2|tail -n1|sed "s/ /,/g") 
 box=$(eBoxSize.pl ref-lig.mol2|head -n1|awk '{ print ($1/3.75)*10 }') #Grid spacing, you can chance it.
 
-#Preparinge receptor
+#Preparing receptor
 $mglbin/pythonsh $script/prepare_receptor4.py -r receptor.pdb -o receptor.pdbqt
 
 wd=`pwd` #Defining working directory
 mkdir results
+
+#Virtual screening
 for i in `cat list`;do
 mkdir $wd/$i && cd $wd/$i &&
 cp ../receptor.pdbqt . &&
@@ -39,5 +41,12 @@ autodock4 -p complex.dpf -l $i.dlg &&
 cd $wd && cp $i/$i.dlg ../results/ && 
 rm -r $wd/$i &
 nrwait $NR_CPUS
+done
+wait
+
+#Getting results
+for i in `cat list`;do
+score=`grep "DOCKED: USER    Estimated Free Energy of Binding" results/$i.dlg|sort -k9 -g|head -n1|awk '{print $9}'`
+echo "$i $score" >> results.dat
 done
 wait
