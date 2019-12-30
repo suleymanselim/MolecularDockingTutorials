a4e5245a738ec5edabea6ac2b0d7f4eac19a43ac
Suleyman Selim <suleyman.selim@mail.com>
2019-12-30 10:40:20 +0000

    Add files via upload

--- autodock.sh
+++ autodock.sh
@@ -2,6 +2,7 @@
 export ligands="~/ligands"
 export mglbin="~/MGLTools-1.5.6/bin"
 export script="~/MGLTools-1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24"
+home=`pwd` 	#Working directory
 
 function nrwait() {
 	local nrwait_my_arg
@@ -20,33 +21,30 @@ NR_CPUS=8		#Number of CPU
 #All settings are used as default options.
 
 #Getting binding center and box size using eBoxSize.pl
-xyz=$(eBoxSize.pl ref-lig.mol2|tail -n1|sed "s/ /,/g") 
-box=$(eBoxSize.pl ref-lig.mol2|head -n1|awk '{ print ($1/3.75)*10 }') #Grid spacing, you can chance it.
+xyz=$(perl $home/eBoxSize.pl $home/ref-lig.mol2|tail -n1|sed "s/ /,/g") 
+box=`printf '%.0f\n' $(perl $home/eBoxSize.pl $home/ref-lig.mol2|head -n1|awk '{ print ($1/3.75)*10 }')` #Grid spacing (3.75), you can chance it.
 
 #Preparing receptor
 $mglbin/pythonsh $script/prepare_receptor4.py -r receptor.pdb -o receptor.pdbqt
 
-wd=`pwd` #Defining working directory
-mkdir results
-
 #Virtual screening
-for i in `cat list`;do
-mkdir $wd/$i && cd $wd/$i &&
-cp ../receptor.pdbqt . &&
-$mglbin/pythonsh $script/prepare_ligand4.py -l ../ligands/${i}.mol2 -o ligand.pdbqt &&
+for i in `cat $home/list`;do
+mkdir $home/$i && cd $home/$i &&
+cp $home/receptor.pdbqt . &&
+$mglbin/pythonsh $script/prepare_ligand4.py -l $home/ligands/${i}.mol2 -o ligand.pdbqt &&
 $mglbin/pythonsh $script/prepare_gpf4.py -l ligand.pdbqt -r receptor.pdbqt -p npts="$box,$box,$box" -p gridcenter="$xyz" -o complex.gpf &&
 $mglbin/pythonsh $script/prepare_dpf4.py -l ligand.pdbqt -r receptor.pdbqt -o complex.dpf &&
 autogrid4 -p complex.gpf -l $i.glg &&
 autodock4 -p complex.dpf -l $i.dlg && 
-cd $wd && cp $i/$i.dlg ../results/ && 
-rm -r $wd/$i &
+cd $home && cp $i/$i.dlg $home/results/autodock && 
+rm -r $i &
 nrwait $NR_CPUS
 done
 wait
 
 #Getting results
 for i in `cat list`;do
-score=`grep "DOCKED: USER    Estimated Free Energy of Binding" results/$i.dlg|sort -k9 -g|head -n1|awk '{print $9}'`
-echo "$i $score" >> results.dat
+score=`grep "DOCKED: USER    Estimated Free Energy of Binding" results/autodock/$i.dlg|sort -k9 -g|head -n1|awk '{print $9}'`
+echo "$i $score" >> $home/autodock-results.dat
 done
 wait
